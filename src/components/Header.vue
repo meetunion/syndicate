4726e0918b06cd70b267a474ba61726e529bd2dd
Tiago Alves <alvesjtiago@gmail.com>
2019-05-10 00:02:18 +0100

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <img :src="coverImageURL" alt="" />
+    <h1>{{ title }}</h1>
+    <div>{{ description }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+    description: String,
+    coverImageURL: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <Header
+      :title="title"
+      :description="description"
+      :coverImageURL="coverImageURL"
+    />
+    <qrcode
+      :value="ethAddress"
+      :options="{ width: 200 }"
+      v-if="ethAddress"
+    ></qrcode>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Api from '../services/ApiManager'
+import Header from '@/components/Header'
+import qrcode from '@chenfengyuan/vue-qrcode'
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
+  name: 'Home',
+  components: { Header, qrcode },
+  data: () => {
+    return {
+      title: '',
+      description: '',
+      coverImageURL: '',
+      ethAddress: ''
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      Api.get('union').then(response => {
+        console.log(response.data)
+        this.title = response.data.title
+        this.description = response.data.description
+        this.coverImageURL = response.data.cover_image_url
+        this.ethAddress = response.data.eth_address
+      })
+    }
   }
-};
+}
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
